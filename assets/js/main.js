b540cd8e284f7894cc9b721c0c07d89b34cc9784
Richard Lucas <richardluca@gmail.com>
2022-04-15 01:23:28 +0200

    navbar updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,7 @@
-console.log("Testing...");
+const toggleBtn = document.querySelector(".btn-toggle");
+const navListItems = document.querySelectorAll(".page-links li a ");
+
+// Works
+toggleBtn.addEventListener("click", function () {
+  navListItems.forEach((item) => item.classList.toggle("show-hide"));
+});
